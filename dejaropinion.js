096b8a11bb89027bb2ee49d924936fcad9c92a0a
Luciana Taranto <luli.taranto.99@gmail.com>
2023-03-08 17:45:19 -0300

    evento click o submit

--- dejaropinion.js
+++ dejaropinion.js
@@ -1,8 +1,12 @@
+// OPINION PARA FORM 1
+
 let opiniones = document.getElementById("boton_enviar")
 
-opiniones.addEventListener("click", function(){
+opiniones.addEventListener("submit", (e) => {
+
+    e.preventDefault
     
-    let opinion_input = document.getElementById("opinion_input")
+    let opinion_input = document.getElementById("opinion_input").value
 
     console.log (opinion_input)
 
@@ -10,3 +14,78 @@ opiniones.addEventListener("click", function(){
 
 let opinionGuardada = localStorage.setItem ("opinionGuardada", JSON.stringify(opiniones))
     console.log (opinionGuardada)
+
+// OPINION PARA FORM 2
+
+let opiniones1 = document.getElementById("boton_enviar-1")
+
+opiniones1.addEventListener("click", function(){
+    
+    let opinion_input1 = document.getElementById("opinion_input-1").value
+
+    console.log (opinion_input1)
+
+})
+
+let opinionGuardada1 = localStorage.setItem ("opinionGuardada1", JSON.stringify(opiniones1))
+    console.log (opinionGuardada1)
+
+// OPINION PARA FORM 3
+
+let opiniones2 = document.getElementById("boton_enviar-2")
+
+opiniones2.addEventListener("click", function(){
+    
+    let opinion_input2 = document.getElementById("opinion_input-2").value
+
+    console.log (opinion_input2)
+
+})
+
+let opinionGuardada2 = localStorage.setItem ("opinionGuardada2", JSON.stringify(opiniones2))
+    console.log (opinionGuardada2)
+
+// OPINION PARA FORM 4
+
+let opiniones3 = document.getElementById("boton_enviar-3")
+
+opiniones3.addEventListener("click", function(){
+    
+    let opinion_input3 = document.getElementById("opinion_input-3").value
+
+    console.log (opinion_input3)
+
+})
+
+let opinionGuardada3 = localStorage.setItem ("opinionGuardada3", JSON.stringify(opiniones3))
+    console.log (opinionGuardada3)
+
+// OPINION PARA FORM 5
+
+let opiniones4 = document.getElementById("boton_enviar-4")
+
+opiniones4.addEventListener("click", function(){
+    
+    let opinion_input4 = document.getElementById("opinion_input-4").value
+
+    console.log (opinion_input4)
+
+})
+
+let opinionGuardada4 = localStorage.setItem ("opinionGuardada4", JSON.stringify(opiniones4))
+    console.log (opinionGuardada4)
+
+// OPINION PARA FORM 6
+
+let opiniones5 = document.getElementById("boton_enviar-5")
+
+opiniones5.addEventListener("click", function(){
+    
+    let opinion_input5 = document.getElementById("opinion_input-5").value
+
+    console.log (opinion_input5)
+
+})
+
+let opinionGuardada5 = localStorage.setItem ("opinionGuardada5", JSON.stringify(opiniones5))
+    console.log (opinionGuardada5)
--- obteneropinion.js
+++ obteneropinion.js
@@ -1,3 +1,6 @@
+
+// OPINION OBTENIDA FORM 1
+
 let opinionObtenida = localStorage.getItem ("opinionGuardada")
     console.log (opinionObtenida)
 
@@ -14,6 +17,111 @@ let botones_borrar = document.querySelectorAll(".btn_borrar");
 console.log( botones_borrar);
 
 for( let boton of botones_borrar ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// OPINION OBTENIDA FORM 2
+
+let opinionObtenida1 = localStorage.getItem ("opinionGuardada1")
+    console.log (opinionObtenida1)
+
+let lista1 = document.createElement("ul")
+
+let li1 = document.createElement("li")
+    
+    li1.innerHTML = `<span>${opinionObtenida1}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista1.append( li1 );
+
+let botones_borrar1 = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar1);
+
+for( let boton of botones_borrar1 ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// OPINION OBTENIDA FORM 3
+
+let opinionObtenida2 = localStorage.getItem ("opinionGuardada2")
+    console.log (opinionObtenida1)
+
+let lista2 = document.createElement("ul")
+
+let li2 = document.createElement("li")
+    
+    li2.innerHTML = `<span>${opinionObtenida2}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista2.append( li2 );
+
+let botones_borrar2 = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar2);
+
+for( let boton of botones_borrar1 ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// OPINION OBTENIDA FORM 4
+
+let opinionObtenida3 = localStorage.getItem ("opinionGuardada3")
+    console.log (opinionObtenida3)
+
+let lista3 = document.createElement("ul")
+
+let li3 = document.createElement("li")
+    
+    li3.innerHTML = `<span>${opinionObtenida3}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista3.append( li3 );
+
+let botones_borrar3 = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar3);
+
+for( let boton of botones_borrar3 ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// OPINION OBTENIDA FORM 5
+
+let opinionObtenida4 = localStorage.getItem ("opinionGuardada4")
+    console.log (opinionObtenida4)
+
+let lista4 = document.createElement("ul")
+
+let li4 = document.createElement("li")
+    
+    li4.innerHTML = `<span>${opinionObtenida4}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista4.append( li4 );
+
+let botones_borrar4 = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar4);
+
+for( let boton of botones_borrar4 ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// OPINION OBTENIDA FORM 6
+
+let opinionObtenida5 = localStorage.getItem ("opinionGuardada5")
+    console.log (opinionObtenida5)
+
+let lista5 = document.createElement("ul")
+
+let li5 = document.createElement("li")
+    
+    li5.innerHTML = `<span>${opinionObtenida5}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista1.append( li5 );
+
+let botones_borrar5 = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar5);
+
+for( let boton of botones_borrar5 ){
 boton.addEventListener("click" , borrar_elemento );
 }
 
