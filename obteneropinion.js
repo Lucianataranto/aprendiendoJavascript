7999c30540d5d86960c71b354ed051840133a2f3
Luciana Taranto <luli.taranto.99@gmail.com>
2023-03-08 15:23:10 -0300

    revisando eventos y localstorage

---
+++ obteneropinion.js
@@ -0,0 +1,55 @@
+let opinionObtenida = localStorage.getItem ("opinionGuardada")
+    console.log (opinionObtenida)
+
+let lista = document.createElement("ul")
+
+let li = document.createElement("li")
+    
+    li.innerHTML = `<span>${opinionObtenida}</span>
+    <button class="btn_borrar">BORRAR</button>`
+    lista.append( li );
+
+let botones_borrar = document.querySelectorAll(".btn_borrar");
+
+console.log( botones_borrar);
+
+for( let boton of botones_borrar ){
+boton.addEventListener("click" , borrar_elemento );
+}
+
+// if(opiniones) {
+//     opiniones.addEventListener("click", function(){
+    
+//     let opinion_input = document.getElementById("opinion_input")
+    
+//     li.innerHTML = `<span>${opinion_input.value}</span>
+//     <a href="./opiniones.html"> Ver mi opinión </a>
+//     <button class="btn_borrar">BORRAR</button>`
+
+//     lista.append( li );
+
+//     localStorage.setItem("opinionGuardada", opinion_input)
+
+//     // function obtenerOpinion () {
+//     //     let opinionObtenida = localStorage.getItem ("opinionGuardada")
+//     //     console.log (opinionObtenida)
+//     // }
+// })
+// }
+
+// for (let i = 0; i < localStorage.length; i + 1) {
+//     let partida = localStorage.key(i)
+//     let opinionObtenida = localStorage.getItem (partida)
+//     console.log (opinionObtenida)
+// }
+
+// function borrar_elemento(e){
+//     console.log("BORRAR ESTE ELEMENTO: " , e.target);
+
+//     let nodo_hijo = e.target;
+//     let nodo_padre = nodo_hijo.parentNode;
+
+//     console.log(nodo_padre);
+
+//     nodo_padre.remove(); 
+// }
